a1921e99a8fff277c768ad78df572ff019fae37e
Ehsan Zangene <zangeneh.ehsan@gmail.com>
2025-05-27 15:49:21 +0300

    here

---
+++ first_session_1.R
@@ -0,0 +1,171 @@
+
+
+a = 31
+
+# +
+#   
+# -
+#   
+#   *
+#   
+#   /
+#   
+#   ^
+#   
+  
+
+5^4  
+  
+
+<
+  
+<=
+  
+>
+  
+>=
+  
+  
+==
+  
+!=
+  
+# ligical operators
+
+!  
+
+&
+  
+|
+  
+  #  membership operator
+%in%  
+  
+4 %in% 1:10 
+
+4 == 1:10
+
+10 = 20
+10 == 20
+
+10 - 20
+
+
+1 + 7
+
+13 - 10
+
+4 * 6
+
+12 / 3
+
+
+#  some simple math 
+
+5^4
+
+16^(1/2)
+
+3^2 - 5/2
+(3^2 - 5)/2
+
+#  T/F
+2 + 2 == 4
+2 + 2 == 5
+
+
+a <- 1 + 2
+a
+
+a <- 10
+a
+
+b <- 5
+
+c <- a + b
+
+#  choosing names
+
+age class 2 <- 32
+age_class_2 <- 32
+
+
+# objects
+ab <- 1:10
+
+# attibutes
+length(ab)
+class(ab)
+
+dim(ab)
+
+
+numeric_vector <- c(1, 2, 3)
+
+character_vector <- c("apple", "cherry", "banana")
+
+character_vector[2]
+
+numeric_vector <= 2
+
+# matrix 
+matrix_variable <- matrix(NA, nrow = 2, ncol = 3)
+
+# list
+list_verstility <- list(numeric_vector, character_vector)
+
+# some essential funcs
+head()
+tail()
+nrow(matrix_variable)
+dim(matrix_variable)
+
+ncol(matrix_variable)
+
+# functions
+round()
+
+# packages
+library()
+
+# error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
+  
